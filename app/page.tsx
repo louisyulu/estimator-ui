4836283248d904332217ea935722a695d0eb893a
Louis Lu <louisyulu@gmail.com>
2024-04-20 20:17:12 -0400

    call REST API to increase the count

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,29 @@
 'use client'
 
-import { log } from 'console';
-import { FormEvent } from 'react'
+import { useState } from 'react'
 
 export default function Home() {
-  async function onSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-    // const formData = new FormData(event.currentTarget);
-    // console.log(formData);
-    const response = await fetch('/api/echo', {
+  const [count, setCount] = useState(0);
+  const [echoBack, setEchoBack] = useState('');
+
+  const handleClick = async () => {
+    const msg = `count = ${count}`;
+    const resp = await fetch('api/echo', {
       method: 'POST',
-      body: { msg: 'hi there!' }, //formData,
+      body: msg,
     })
-    // Handle response if necessary
-    const data = await response.json()
-    console.log(data);
-    // ...
+    const json = await resp.json();
+    setEchoBack(JSON.stringify(json));
+    setCount(count + 1)
   }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form onSubmit={onSubmit} className="max-w-sm mx-auto">
-        <div className="mb-5">
-          <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Message:</label>
-          <input type="input" id="message" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-        </div>
-        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-      </form>
+      <div className="mb-5">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Click the button to increase the count by the server</label>
+        <button onClick={handleClick} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send</button>
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{echoBack}</label>
+      </div>
     </main>
   );
 }
--- pages/api/echo.ts
+++ pages/api/echo.ts
@@ -4,11 +4,8 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    console.log(req.body);
-    const msg = req.body['msg'];
+    const msg = req.body;
     const response = await fetch(`http://localhost:8080/echo?msg=${msg}`);
-    console.log(response);
     const data = await response.json();
     res.status(200).send(data);
-    
 }
