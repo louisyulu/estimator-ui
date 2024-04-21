d68b8677afae64d68d60a401256fa1555d1f8e39
Louis Lu <louisyulu@gmail.com>
2024-04-20 20:26:24 -0400

    update display

--- app/page.tsx
+++ app/page.tsx
@@ -20,9 +20,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mb-5">
-        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Click the button to increase the count by the server</label>
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Click the button to send REST API call</label>
         <button onClick={handleClick} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send</button>
-        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{echoBack}</label>
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Response -- {echoBack}</label>
       </div>
     </main>
   );
