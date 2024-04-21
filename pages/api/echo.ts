91103b38214136898c8d7b4ff32f583eed55c1d6
Louis Lu <louisyulu@gmail.com>
2024-04-21 00:32:58 -0400

    pointing to the azure server

--- pages/api/echo.ts
+++ pages/api/echo.ts
@@ -5,7 +5,8 @@ export default async function handler(
     res: NextApiResponse
 ) {
     const msg = req.body;
-    const response = await fetch(`http://localhost:8080/echo?msg=${msg}`);
+    // const response = await fetch(`http://localhost:8080/echo?msg=${msg}`);
+    const response = await fetch(`https://louis-poc-rules-java.azurewebsites.net/echo?msg=${msg}`);
     const data = await response.json();
     res.status(200).send(data);
 }
