a2583636e5a6774136a0c1657192f74b3ba781db
Louis Lu <louisyulu@gmail.com>
2024-04-30 10:37:50 -0400

    adjust display

--- app/adult-rule/page.tsx
+++ app/adult-rule/page.tsx
@@ -1,13 +1,13 @@
-"use client"
+"use client";
 
-import { useState } from 'react'
+import { useState } from "react";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { Button } from "@/components/ui/button"
-import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
 import {
   Form,
   FormControl,
@@ -15,17 +15,17 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 const formSchema = z.object({
   name: z.string().min(2, {
     message: "name must be at least 2 characters.",
   }),
   age: z.coerce.number().min(1).max(100),
-})
+});
 
 export default function PersonForm() {
-  const [resp, setResp] = useState('');
+  const [resp, setResp] = useState("");
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -33,22 +33,25 @@ export default function PersonForm() {
       name: "John",
       age: 18,
     },
-  })
+  });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values)
-    const resp = await fetch('api/adult-rule', {
-      method: 'POST',
+    console.log(values);
+    const resp = await fetch("api/adult-rule", {
+      method: "POST",
       body: JSON.stringify(values),
-    })
+    });
     const json = await resp.json();
     setResp(JSON.stringify(json));
-  }
+  };
 
   return (
-    <div>
+    <div className="flex min-h-screen flex-col items-center p-10 gap-3">
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="w-1/4 ml-20 mt-8 space-y-3">
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-1/4 ml-20 mt-8 space-y-3"
+        >
           <FormField
             control={form.control}
             name="name"
@@ -75,14 +78,16 @@ export default function PersonForm() {
               </FormItem>
             )}
           />
-          <Button type="submit" className="text-white bg-blue-700  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <Button
+            type="submit"
+            className="text-white bg-blue-700  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
             Submit
           </Button>
         </form>
       </Form>
-      <div className="w-1/4 ml-20 mt-8 space-y-3">
-        <Label>Response -- {resp}</Label>
-      </div>
+      <label>Response:</label>
+      <div>{resp}</div>
     </div>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,33 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
+import { useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(0);
-  const [echoBack, setEchoBack] = useState('');
+  const [echoBack, setEchoBack] = useState("");
 
   const handleClick = async () => {
     const msg = `count = ${count}`;
-    const resp = await fetch('api/echo', {
-      method: 'POST',
+    const resp = await fetch("api/echo", {
+      method: "POST",
       body: msg,
-    })
+    });
     const json = await resp.json();
     setEchoBack(JSON.stringify(json));
-    setCount(count + 1)
-  }
+    setCount(count + 1);
+  };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="mb-5">
-        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Click the button to send REST API call</label>
-        <button onClick={handleClick} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          Send
-        </button>
-        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Response -- {echoBack}</label>
-      </div>
+    <main className="flex min-h-screen flex-col items-center p-10 gap-3">
+      <label>Click the button to send REST API call</label>
+      <button
+        onClick={handleClick}
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Send
+      </button>
+      <label>Response:</label>
+      <div>{echoBack}</div>
     </main>
   );
 }
